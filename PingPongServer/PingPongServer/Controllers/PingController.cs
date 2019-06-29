a83ca18e9a20decd18a6ffe8db9d3eeb1e5db16a
Yairsno1 <yairsno1@gmail.com>
2019-06-29 11:29:44 +0300

    Server - Initial check-in

---
+++ PingPongServer/PingPongServer/Controllers/PingController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PingPongServer.Controllers
+{
+  [Route("api/v1/[controller]")]
+  [ApiController]
+  public class PingController : ControllerBase
+  {
+    // GET: api/v1/<controller>
+    [HttpGet]
+    public ActionResult<Dictionary<string, string>> Get()
+    {
+      return new Dictionary<string, string> { { "pong", "pong!"} };
+    }
+  }
+}
